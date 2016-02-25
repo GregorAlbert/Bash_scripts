7e1fcc43f5f00e111b68a531610482a166809764
Gregor Albert <gregoralbert@gmail.com>
2016-02-25 09:34:30 +0100

    Update directory_cleaner.sh
    Variable filetype length can be used

--- directory_cleaner.sh
+++ directory_cleaner.sh
@@ -5,22 +5,23 @@
 path=/home/
 	#Files older than $days will be deleted
 days=7
-	#File types with length 3 in $file_types will be removed
+	#File types with in $file_types will be removed
 file_types=(xls pdf txt)
-
+#
 
 limit_time=`date +%s -d "-$days days"`
 FILES=$path/*
 
 for file in $FILES ; do	
 	for i in "${file_types[@]}" ; do
-	    if [ ${file: -4} == ".$i" ] ; then
+		filename=$(basename "$file")
+	    if [ "${filename##*.}" == "$i" ] ; then
 		    file_time=`stat -c %Z "$file"`
 		    if [ $file_time -le $limit_time ] ; then
-		        echo "Deleting $file"
-				rm "$file"
+		         echo "Deleting $file"
+				 rm "$file"
 		    fi		
-      fi
+            fi
 	done
 done
 
